6d736c793191915d443b352165fb5baac51f3733
Truong-Dio <85091699+Truong-Dio@users.noreply.github.com>
2024-10-22 19:55:31 +0700

    update transaction api

---
+++ Fengshui-Koi-Consultation-System-Common/repository/src/main/java/com/fengshui/common/repository/postgresql/dto/TransactionDTO.java
@@ -0,0 +1,21 @@
+package com.fengshui.common.repository.postgresql.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+
+public class TransactionDTO {
+    private UUID transaction_id;
+    private LocalDateTime createdAt;
+    private String userId;
+    private String adsPackage;
+    private double price;
+ //   private boolean deleted;
+}

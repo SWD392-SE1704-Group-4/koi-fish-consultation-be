1e5fe8c96ae6106fa96e657e693de6a7ff30cb6f
LerclercDuong <89032831+LerclercDuong@users.noreply.github.com>
2024-10-28 02:57:34 +0700

    Feat: findAdsByCreator

--- Fengshui-Koi-Consultation-System-Common/repository/src/main/java/com/fengshui/common/repository/postgresql/IAdvertisementRepository.java
+++ Fengshui-Koi-Consultation-System-Common/repository/src/main/java/com/fengshui/common/repository/postgresql/IAdvertisementRepository.java
@@ -1,11 +1,17 @@
 package com.fengshui.common.repository.postgresql;
 
 import com.fengshui.common.repository.postgresql.entities.AdvertisementEntity;
+import com.fengshui.common.repository.postgresql.entities.FishPondEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IAdvertisementRepository extends JpaRepository<AdvertisementEntity, UUID> {
+    @Query("SELECT a FROM AdvertisementEntity a WHERE a.postedBy.id = :appUserId")
+    List<AdvertisementEntity> findAdsByCreator(@Param("appUserId") UUID appUserId);
 }

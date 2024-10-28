d28b8a2123f7b76699ef7996c4bb3f768bcb1d94
Truong-Dio <85091699+Truong-Dio@users.noreply.github.com>
2024-10-29 05:40:20 +0700

    Done AdsPackageMapper to implement

--- Fengshui-Koi-Consultation-System-Common/repository/src/main/java/com/fengshui/common/repository/postgresql/mapper/AdsPackageMapper.java
+++ Fengshui-Koi-Consultation-System-Common/repository/src/main/java/com/fengshui/common/repository/postgresql/mapper/AdsPackageMapper.java
@@ -1,4 +1,31 @@
 package com.fengshui.common.repository.postgresql.mapper;
 
+import com.fengshui.common.repository.postgresql.dto.AdsPackageDTO;
+import com.fengshui.common.repository.postgresql.entities.AdsPackageEntity;
+
 public class AdsPackageMapper {
+
+    public static AdsPackageDTO toDTO(AdsPackageEntity entity){
+        if(entity == null){return null;}
+        return AdsPackageDTO.builder()
+                .adsPackageId(entity.getId())
+                .adsPackageName(entity.getPackageName())
+                .packagePrice(entity.getPackagePrice())
+                .packageValue(entity.getPackageValue())
+                .packagePriority(entity.getPackagePriority())
+                .build();
+    }
+
+    public static AdsPackageEntity toEntity(AdsPackageDTO dto){
+        if(dto == null){return null;}
+        return AdsPackageEntity.builder()
+                .id(dto.getAdsPackageId())
+                .packageName(dto.getAdsPackageName())
+                .packagePrice(dto.getPackagePrice())
+                .packageValue(dto.getPackageValue())
+                .packagePriority(dto.getPackagePriority())
+                .build();
+    }
+
+
 }

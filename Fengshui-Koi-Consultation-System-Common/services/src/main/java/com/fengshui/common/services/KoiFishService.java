31f13a9abdc240dd9c9082971bb4fae8bf02f7c2
LerclercDuong <89032831+LerclercDuong@users.noreply.github.com>
2024-11-04 19:36:53 +0700

    Feat: Koi fish add feature

--- Fengshui-Koi-Consultation-System-Common/services/src/main/java/com/fengshui/common/services/KoiFishService.java
+++ Fengshui-Koi-Consultation-System-Common/services/src/main/java/com/fengshui/common/services/KoiFishService.java
@@ -1,17 +1,12 @@
 package com.fengshui.common.services;
 
 import com.fengshui.common.repository.postgresql.entities.KoiFishEntity;
-import com.fengshui.common.shared.Request.KoiFish.CreateKoiFishRequestModel;
-import com.fengshui.common.shared.Request.KoiFish.DeleteKoiFishRequestModel;
-import com.fengshui.common.shared.Request.KoiFish.GetKoiFishRequestModel;
-import com.fengshui.common.shared.Request.KoiFish.UpdateKoiFishRequestModel;
+import com.fengshui.common.shared.Request.KoiFish.*;
 import com.fengshui.common.shared.Response.BaseResponseModel;
-import com.fengshui.common.shared.Response.KoiFish.CreateKoiFishResponseModel;
-import com.fengshui.common.shared.Response.KoiFish.DeleteKoiFishResponseModel;
-import com.fengshui.common.shared.Response.KoiFish.GetKoiFishResponseModel;
-import com.fengshui.common.shared.Response.KoiFish.UpdateKoiFishResponseModel;
+import com.fengshui.common.shared.Response.KoiFish.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -20,4 +15,5 @@ public interface KoiFishService {
     public ResponseEntity<GetKoiFishResponseModel> getListKoiFish(GetKoiFishRequestModel requestModel);
     public ResponseEntity<UpdateKoiFishResponseModel> updateKoiFish(UpdateKoiFishRequestModel requestModel);
     public ResponseEntity<DeleteKoiFishResponseModel> deleteKoiFish(DeleteKoiFishRequestModel requestModel);
+    public ResponseEntity<GetKoiFishByElementNameResponseModel> getListKoiFishByElementName(GetKoiFishByElementNameRequestModel requestModel);
 }
